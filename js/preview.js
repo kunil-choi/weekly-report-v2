c947ec3dd00a645b13da3a487984241fc4333d8e
kunil-choi <gaegoo99@gmail.com>
2026-04-09 16:04:47 +0900

    Update preview.js

--- js/preview.js
+++ js/preview.js
@@ -25,56 +25,80 @@ function getReportFileName(){
 function buildPreview(){
   var a=document.getElementById('prevArea');var h='';
   var lwRange=fmtShort(S.lws)+'~'+fmtShort(S.lwe);
+  var twRange=fmtShort(S.tws)+'~'+fmtShort(S.twe);
   var title=getReportTitle();
 
   h+='<h3>■ '+title+'</h3>';
 
-  /* 일정 + 조회수 테이블 */
-  h+='<table class="tbl" id="t1"><tr><th>날짜</th><th>업로드 및 예정 아이템</th><th>스튜디오 녹화 일정</th><th>조회수</th></tr>';
+  /* ===== 지난주 실적 테이블 ===== */
+  /* [수정1] 지난주: 업로드는 유튜브 영상 섹션에 이미 표시되므로 녹화+특이사항+조회수만 표시 */
+  h+='<h4 style="color:#818cf8;margin:14px 0 6px"><span class="wl last">지난주</span> '+lwRange+'</h4>';
+  h+='<table class="tbl" id="t1a"><tr><th>날짜</th><th>스튜디오 녹화 일정</th><th>특이사항</th><th>조회수</th></tr>';
   var lwD=daysIn(S.lws,S.lwe);
   for(var di=0;di<lwD.length;di++){
     var d=lwD[di];var dateLabel=fmtShort(d)+'('+DK[d.getDay()]+')';
     var sch=S.lastSch.filter(function(r){return sameDay(r._date,d)});
-    var upload='-',record='-',views='-';
+    var record='-', noteVal='-', views='-';
     if(sch.length){
-      var uploads=[],records=[];
+      var records=[], notes=[];
       for(var si=0;si<sch.length;si++){
-        if(sch[si].uploadItem)uploads.push(sch[si].uploadItem);
-        if(sch[si].studioRecord)records.push(sch[si].studioRecord);
+        if(sch[si].studioRecord) records.push(sch[si].studioRecord);
+        if(sch[si].note) notes.push(sch[si].note);
       }
-      upload=uploads.length?uploads.join(', '):'-';
       record=records.length?records.join(', '):'-';
+      noteVal=notes.length?notes.join(', '):'-';
+    }
+    /* docx에서 추출한 _prevSchedule 보충 (GAS 데이터가 없을 때) */
+    if(record==='-' && S._prevSchedule && S._prevSchedule.length){
+      for(var pi=0;pi<S._prevSchedule.length;pi++){
+        var ps=S._prevSchedule[pi];
+        if(ps.date && ps.date.indexOf((d.getMonth()+1)+'/'+d.getDate())>-1){
+          if(ps.rec) record=ps.rec;
+          if(ps.note) noteVal=ps.note;
+        }
+      }
     }
     var vid=S.yt.filter(function(v){return sameDay(v._date,d)});
-    if(vid.length)views=Number(vid[0].views).toLocaleString();
-    h+='<tr><td>'+dateLabel+'</td><td contenteditable="true">'+upload+'</td><td contenteditable="true">'+record+'</td><td contenteditable="true">'+views+'</td></tr>';
+    if(vid.length) views=Number(vid[0].views).toLocaleString();
+    h+='<tr><td>'+dateLabel+'</td><td contenteditable="true">'+record+'</td><td contenteditable="true">'+noteVal+'</td><td contenteditable="true">'+views+'</td></tr>';
   }
+  h+='</table>';
+
+  /* ===== 이번주 계획 테이블 ===== */
+  /* [수정2] 이번주: 같은날 복수 일정 → 콤마 구분 */
+  h+='<h4 style="color:#34d399;margin:14px 0 6px"><span class="wl this">이번주</span> '+twRange+'</h4>';
+  h+='<table class="tbl" id="t1b"><tr><th>날짜</th><th>업로드 및 예정 아이템</th><th>스튜디오 녹화 일정</th><th>특이사항</th></tr>';
   var twD=daysIn(S.tws,S.twe);
   for(var di=0;di<twD.length;di++){
     var d=twD[di];var dateLabel=fmtShort(d)+'('+DK[d.getDay()]+')';
     var sch=S.thisSch.filter(function(r){return sameDay(r._date,d)});
-    var upload='-',record='-';
+    var upload='-',record='-',noteVal='-';
     if(sch.length){
-      var uploads=[],records=[];
+      var uploads=[],records=[],notes=[];
       for(var si=0;si<sch.length;si++){
-        if(sch[si].uploadItem)uploads.push(sch[si].uploadItem);
-        if(sch[si].studioRecord)records.push(sch[si].studioRecord);
+        if(sch[si].uploadItem) uploads.push(sch[si].uploadItem);
+        if(sch[si].studioRecord){
+          var recEntry = sch[si].recordTime ? (sch[si].recordTime+' '+sch[si].studioRecord) : sch[si].studioRecord;
+          records.push(recEntry);
+        }
+        if(sch[si].note) notes.push(sch[si].note);
       }
       upload=uploads.length?uploads.join(', '):'-';
       record=records.length?records.join(', '):'-';
+      noteVal=notes.length?notes.join(', '):'-';
     }
-    h+='<tr><td>'+dateLabel+'</td><td contenteditable="true">'+upload+'</td><td contenteditable="true">'+record+'</td><td>-</td></tr>';
+    h+='<tr><td>'+dateLabel+'</td><td contenteditable="true">'+upload+'</td><td contenteditable="true">'+record+'</td><td contenteditable="true">'+noteVal+'</td></tr>';
   }
   h+='</table>';
 
-  /* 채널 현황 */
+  /* ===== 채널 현황 ===== */
   h+='<h3>■ 채널 현황 ('+lwRange+')</h3>';
   h+='<table class="tbl" id="t2"><tr><th></th><th>조회수</th><th>구독자</th><th>예상 수익</th><th>재생 기반 CPM</th><th>RPM</th><th>노출 클릭률</th><th>평균 시청 지속 시간</th></tr>';
   h+='<tr><td><b>지난주</b></td><td contenteditable="true">'+(S.ch.views||'')+'</td><td contenteditable="true">'+(S.ch.subs||'')+'</td><td contenteditable="true">'+(S.ch.rev||'')+'</td><td contenteditable="true">'+(S.ch.cpm||'')+'</td><td contenteditable="true">'+(S.ch.rpm||'')+'</td><td contenteditable="true">'+(S.ch.ctr||'')+'</td><td contenteditable="true">'+(S.ch.avg||'')+'</td></tr>';
   h+='<tr><td><b>지지난주</b></td><td contenteditable="true">'+(S.ch2.views||'')+'</td><td contenteditable="true">'+(S.ch2.subs||'')+'</td><td contenteditable="true">'+(S.ch2.rev||'')+'</td><td contenteditable="true">'+(S.ch2.cpm||'')+'</td><td contenteditable="true">'+(S.ch2.rpm||'')+'</td><td contenteditable="true">'+(S.ch2.ctr||'')+'</td><td contenteditable="true">'+(S.ch2.avg||'')+'</td></tr>';
   h+='</table>';
 
-  /* 콘텐츠 유형 */
+  /* ===== 콘텐츠 유형 ===== */
   h+='<h3>■ 콘텐츠 유형별 ('+lwRange+')</h3>';
   h+='<table class="tbl" id="t3"><tr><th></th><th>조회수</th><th>시청 시간</th></tr>';
   h+='<tr><td><b>지난주 동영상</b></td><td contenteditable="true">'+(S.ct.vv||'')+'</td><td contenteditable="true">'+(S.ct.vw||'')+'</td></tr>';
@@ -83,9 +107,9 @@ function buildPreview(){
   h+='<tr><td><b>지지난주 Shorts</b></td><td contenteditable="true">'+(S.ct2.sv||'')+'</td><td contenteditable="true">'+(S.ct2.sw||'')+'</td></tr>';
   h+='</table>';
 
-  /* 구독자 & 메모 */
+  /* ===== 구독자 & 메모 ===== */
   h+='<h3>■ 구독자 현황</h3>';
-  h+='<p id="pSubs">현재 구독자 수: <b>'+(S.subs||'')+'</b></p>';
+  h+='<p id="pSubs" contenteditable="true">현재 구독자 수: <b>'+(S.subs||'')+'</b></p>';
   h+='<h3>■ 팀원별 특이사항</h3>';
   h+='<table class="tbl" id="t4"><tr><th>팀원</th><th>내용</th></tr>';
   h+='<tr><td>양영은</td><td contenteditable="true">'+(S.nY||'-')+'</td></tr>';
